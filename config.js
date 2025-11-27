3cf9c11d62f7825191321028774a2216d214ac9c
tumu <230271830+tunutz200@users.noreply.github.com>
2025-11-28 01:35:21 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "dark~UI9A3DYL#DV3JyDWQjkrfSI8oSuUZlDivuPSuQtsmc7y0NvKN5uY",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -37,7 +37,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "👍,❤️,😂,😮,�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94771825192",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "25567300835",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "𝙳𝙰𝚁𝙺-𝙺𝙽𝙸𝙶𝙷𝚃",
 // add bot owner name
